0e7384acdab782caeccc37953f809896d967cf7d
ducanh <ducanh20176919@gmail.com>
2024-03-12 00:44:15 +0700

    AnhDDe dựng base FE

--- app/i18n.js
+++ app/i18n.js
@@ -4,17 +4,13 @@ import Backend from 'i18next-http-backend';
 import detector from 'i18next-browser-languagedetector';
 
 import vi from './locales/vi/index';
-import en from './locales/en/index';
 
-const Languages = ['en', 'vi'];
+const Languages = ['vi'];
 
 export const DEFAULT_LOCALE =
   process.env.NODE_ENV === 'production' ? window.SystemConfig.LANG : 'vi';
 
 const resources = {
-  en: {
-    translation: en,
-  },
   vi: {
     translation: vi,
   },
